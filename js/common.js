a6a315dd82d8cf6f18448e219f01e8db94a948c0
mio <mikonuko@gmail.com>
2021-05-13 10:43:50 +0900

    portfolio files

---
+++ js/common.js
@@ -0,0 +1,10 @@
+/* sp_nav_btn
+------------------------------------------------------------*/
+
+$(document).ready(function() {
+
+  $(".sp_nav_btn").click(function() {
+    $(".g_nav_open").slideToggle();
+  });
+
+});
